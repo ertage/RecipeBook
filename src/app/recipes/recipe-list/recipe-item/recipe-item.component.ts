e9a106b1a9129f349e4f9d2ca84a3ae0389408a8
Anastasia Shvetsova <shv.an.vol@gmail.com>
2018-01-19 12:35:53 +0200

    added recepi item component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() name: string;
+  @Input() description: string;
+  @Input() imageUrl: string;
 
   constructor() { }
-
   ngOnInit() {
   }
 
